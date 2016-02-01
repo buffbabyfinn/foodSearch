8caa4555151333d729281fab519803a157d99aa1
Michael Gallegos <mklgallegos@gmail.com>
2016-02-01 09:50:16 -0800

    add mike_restaurant_data.js file to repo

---
+++ js/mike_restaurant_data.js
@@ -0,0 +1,9 @@
+// var restaurant {
+name: "string",
+cuisine: ["string","string","string"]
+rating: integer
+price-range: integer
+location: "string of lat-long?"
+restrictions: [{glutenfree: true/false},{...},{...}]
+menu-items:[{},{},{}]
+}
