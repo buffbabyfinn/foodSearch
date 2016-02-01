d7e589d158a4631e8e61fa019e302dc70395d3f9
Michael Gallegos <mklgallegos@gmail.com>
2016-02-01 10:40:34 -0800

    add Natural Selection restaurant data

--- js/mike_restaurant_data.js
+++ js/mike_restaurant_data.js
@@ -1,9 +1,180 @@
-// var restaurant {
-name: "string",
-cuisine: ["string","string","string"]
-rating: integer
-price-range: integer
-location: "string of lat-long?"
-restrictions: [{glutenfree: true/false},{...},{...}]
-menu-items:[{},{},{}]
+var mike_restaurant_1 = {
+name: "Natural Selection",
+cuisine: ["European"]
+rating: 5
+price_range: 3
+location: "45.560957, -122.634061"
+restrictions: 
+{
+glutenFree: true,
+vegetarian: true,
+pescetarian: true,
+vegan: true,
+dairyFree: true,
+nutFree: false,
+hindu: true,
+jewish: true,
+muslim: true,
+}
+menu_items:[A1,A2,A3,A4,B1,B2,B3,B4]
+}
+
+var A1 = {
+name: "coconut curry soup"
+description: "lemongrass, lime, brussels sprouts, carrot, herb oil"
+ingredients: "lemongrass, lime, brussels sprouts, carrot, herb oil"
+restrictions: 
+{
+glutenFree: true,
+vegetarian: true,
+pescetarian: true,
+vegan: true,
+dairyFree: true,
+nutFree: true,
+hindu: true,
+jewish: true,
+muslim: true, 
+}
+price: 11.25
+picture:
+}
+
+var A2 = {
+name: "belgian endive & treviso salad"
+description: "asian pear, radish, peppers, dried cherries, dijon"
+ingredients: "asian pear, radish, peppers, dried cherries, dijon"
+restrictions: 
+{
+glutenFree: true,
+vegetarian: true,
+pescetarian: true,
+vegan: true,
+dairyFree: true,
+nutFree: true,
+hindu: true,
+jewish: true,
+muslim: true,
+}
+price: 11.25
+picture:   
+}
+
+var A3 = {
+name: "crispy polenta with trumpet royale"
+description: "leek, parsnip, carrot, breakfast radish"
+ingredients: "leek, parsnip, carrot, breakfast radish"
+restrictions: 
+{
+glutenFree: true,
+vegetarian: true,
+pescetarian: true,
+vegan: true,
+dairyFree: true,
+nutFree: true,
+hindu: true,
+jewish: true,
+muslim: true,
+}
+price: 11.25
+picture:   
+}
+
+var A4 = {
+name: "squash cake with caramelized apples"
+description: "apricot, pumpkin mousse, raspberry, almond"
+ingredients: "apricot, pumpkin mousse, raspberry, almond"
+restrictions: 
+{
+glutenFree: true,
+vegetarian: true,
+pescetarian: true,
+vegan: true,
+dairyFree: true,
+nutFree: false,
+hindu: true,
+jewish: true,
+muslim: true,
+}
+price: 11.25
+picture:   
+}
+
+var B1 = {
+name: "chestnut tagliatelle pasta"
+description: "lemon, shallot, balsamic, creamed greens"
+ingredients: "lemon, shallot, balsamic, creamed greens"
+restrictions: 
+{
+glutenFree: false,
+vegetarian: true,
+pescetarian: true,
+vegan: false,
+dairyFree: false,
+nutFree: false,
+hindu: true,
+jewish: true,
+muslim: true,
+}
+price: 11.25
+picture:   
+}
+
+var B2 = {
+name: "citrus & delicata squash salad"
+description: "watercress, olives, dates, creamy sherry dressing"
+ingredients: "watercress, olives, dates, creamy sherry dressing"
+restrictions: 
+{
+glutenFree: true,
+vegetarian: true,
+pescetarian: true,
+vegan: true,
+dairyFree: true,
+nutFree: true,
+hindu: true,
+jewish: true,
+muslim: true,
+}
+price: 11.25
+picture:   
+}
+
+var B3 = {
+name: "creamy herbed risotto"
+description: "braised red cabbage, salsify, sunchoke, cider"
+ingredients: "braised red cabbage, salsify, sunchoke, cider"
+restrictions: 
+{
+glutenFree: true,
+vegetarian: true,
+pescetarian: true,
+vegan: false,
+dairyFree: true,
+nutFree: true,
+hindu: true,
+jewish: true,
+muslim: true,
+}
+price: 11.25
+picture:   
+}
+
+var B4 = {
+name: "lemon tapioca parfait"
+description: "huckleberry, coconut, cinnamon cookie"
+ingredients: "huckleberry, coconut, cinnamon cookie"
+restrictions: 
+{
+glutenFree: true,
+vegetarian: true,
+pescetarian: true,
+vegan: true,
+dairyFree: true,
+nutFree: true,
+hindu: true,
+jewish: true,
+muslim: true,
+}
+price: 11.25
+picture:   
 }
