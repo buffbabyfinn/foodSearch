3da81586340c5d45325b59895f425d02ec5d0635
mollieboots <student@epicodus.com>
2016-02-04 13:39:06 -0800

    front end styling

---
+++ js/menu_item_data.js
@@ -0,0 +1,336 @@
+//this file contains all menu item objects for all restaurant objects
+
+
+//Natural Selection
+var R1M1 = {
+reference: "R1M1",
+name: "coconut curry soup",
+ingredients: "lemongrass, lime, brussels sprouts, carrot, herb oil",
+restrictions: ["glutenFree", "vegetarian", "pescatarian", "vegan", "dairyFree", "nutAllergy", "kosher", "halal"],
+price: 11.25
+}
+
+var R1M2 = {
+reference: "R1M2",
+name: "belgian endive & treviso salad",
+ingredients: "asian pear, radish, peppers, dried cherries, dijon",
+restrictions: ["glutenFree", "vegetarian", "pescatarian", "vegan", "dairyFree", "nutAllergy", "kosher", "halal"],
+price: 11.25
+}
+
+var R1M3 = {
+reference: "R1M3",
+name: "crispy polenta with trumpet royale",
+ingredients: "leek, parsnip, carrot, breakfast radish",
+restrictions: ["glutenFree", "vegetarian", "pescatarian", "vegan", "dairyFree", "nutAllergy", "kosher", "halal"],
+price: 11.25
+}
+
+var R1M4 = {
+reference: "R1M4",
+name: "squash cake with caramelized apples",
+ingredients: "apricot, pumpkin mousse, raspberry, almond",
+restrictions: ["glutenFree", "vegetarian", "pescatarian", "vegan", "dairyFree", "kosher", "halal"],
+price: 11.25
+}
+
+var R1M5 = {
+reference: "R1M5",
+name: "chestnut tagliatelle pasta",
+ingredients: "lemon, shallot, balsamic, creamed greens",
+restrictions: ["vegetarian", "pescatarian", "kosher", "halal"],
+price: 11.25
+}
+
+var R1M6 = {
+reference: "R1M6",
+name: "citrus & delicata squash salad",
+ingredients: "watercress, olives, dates, creamy sherry dressing",
+restrictions: ["glutenFree", "vegetarian", "pescatarian", "vegan", "dairyFree", "nutAllergy", "kosher", "halal"],
+price: 11.25
+}
+
+var R1M7 = {
+reference: "R1M7",
+name: "creamy herbed risotto",
+ingredients: "braised red cabbage, salsify, sunchoke, cider",
+restrictions: ["glutenFree", "vegetarian", "pescatarian", "dairyFree", "nutAllergy", "kosher", "halal"],
+price: 11.25
+}
+
+var R1M8 = {
+reference: "R1M8",
+name: "lemon tapioca parfait",
+ingredients: "huckleberry, coconut, cinnamon cookie",
+restrictions: ["glutenFree", "vegetarian", "pescatarian", "vegan", "dairyFree", "nutAllergy", "kosher", "halal"],
+price: 11.25
+}
+
+//Bunk Sandwiches
+var R2M1 = {
+reference: "R2M1",
+name: "Egg & Cheese",
+ingredients: "egg, cheese, bread roll",
+restrictions: ['vegetarian','pescatarian','nutAllergy','kosher','halal'],
+price: 5,
+picture: true,
+}
+
+var R2M2 = {
+reference: "R2M2",
+name: "Peanut Butter & Jelly",
+ingredients: "peanut butter, nutella, jelly, kettle chips, bread",
+restrictions: ['vegetarian','pescatarian','hindu','kosher','halal','dairyFree'],
+price: 5,
+picture: true
+}
+
+var R2M3 = {
+reference: "R2M3",
+name: "Grilled Tillamook Cheddar",
+ingredients: "cheese, tomato, bread",
+restrictions: ['vegetarian','pescatarian','nutAllergy','hindu','kosher','halal'],
+price: 6,
+picture: true
+}
+
+var R2M4 = {
+reference: "R2M4",
+name: "Apple Cabbage Slaw",
+description: "",
+ingredients: "apple, cabage",
+restrictions: ['vegetarian','pescatarian','nutAllergy','hindu','kosher','halal','vegan', 'glutenFree', 'dairyFree'],
+price: 3,
+picture: true
+}
+
+var R2M5 = {
+reference: "R2M5",
+name: "Marinated Garbanzo Beans",
+description: "",
+ingredients: "grabanzo beans, feta, arugala, and hot peppers",
+restrictions: ['vegetarian','pescatarian','nutAllergy','kosher','halal','vegan'],
+price: 10,
+picture: true
+}
+
+var R2M6 = {
+reference: "R2M6",
+name: "Pork Belly Cubano",
+description: "",
+ingredients: "ham, bread, pork belly and shoulder roast, swiss, mustard, pickles",
+restrictions: ['nutAllergy', 'dairyFree'],
+price: 10,
+picture: true
+}
+
+var R2M7 = {
+reference: "R2M7",
+name: "Oregon Albacore Tuna Melt",
+description: "",
+ingredients: "cheddar, mustard, pickles, bread",
+restrictions: ['pescatarian', 'nutAllergy'],
+price: 11,
+picture: true
+}
+
+var R2M8 = {
+reference: "R2M8",
+name: "Fried Louisiana Gulf Shrimp Po'Boy",
+description: "",
+ingredients: "shrimp, lettuce, tomato, pickles, mayo, bread",
+restrictions: ['pescatarian', 'nutAllergy'],
+price: 11,
+picture: true
+}
+
+
+//Ataula
+
+var R3M1 = {
+reference: "R3M1",
+name: "Nuestra Cesar",
+ingredients: "romaine lettuce, free range chicken, croutons, parmesan",
+restrictions: ['nutAllergy','kosher','halal'],
+price: 12,
+picture: false
+}
+
+var R3M2 = {
+reference: "R3M2",
+name: "Remolacha",
+ingredients: "pickled beets, organic greens, citrus, torpedo onions, walnuts",
+restrictions: ['glutenFree','vegetarian','pescatarian','vegan','dairyFree','hindu','kosher','halal'],
+price: 11,
+picture: false
+}
+
+var R3M3 = {
+reference: "R3M3",
+name: "Boquerones",
+ingredients: "boquerones(sardine), chili-arbequina caviar, pickeled radish, greens",
+restrictions: ['glutenFree', 'pescatarian', 'dairyFree', 'kosher', 'nutAllergy'],
+price: 9,
+picture: false
+}
+
+var R3M4 = {
+reference: "R3M4",
+name: "Nuestras Bravas",
+ingredients: "potato, milk aioli, brava sauce",
+restrictions: ['vegetarian', 'pescatarian', 'nutAllergy', 'kosher', 'glutenFree'],
+price: 8,
+picture: false
+}
+
+var R3M5 = {
+reference: "R3M5",
+name: "Empanadilla",
+ingredients: "braised oxtail, 4 year acorn ham, havarti cheese, red padron powder",
+restrictions: ['nutAllergy', 'glutenFree'],
+price: 8,
+picture: true
+}
+
+var R3M6 = {
+reference: "R3M6",
+name: "Pa amb tomaquet",
+ingredients: "house coca bread, shredded tomato, extra virgin olive oil",
+restrictions: ['nutAllergy', 'vegetarian', 'vegan', 'pescatarian', 'kosher', 'halal', 'hindu', 'dairyFree'],
+price: 6,
+picture: false
+}
+
+var R3M7 = {
+reference: "R3M7",
+name: "Paella Jardinera",
+ingredients: "bomba rice, wild mushrooms, seasonal vegetables",
+restrictions: ['nutAllergy', 'vegetarian', 'vegan', 'pescatarian', 'kosher', 'halal', 'hindu', 'glutenFree', 'dairyFree'],
+price: 32,
+picture: false
+}
+
+var R3M8 = {
+reference: "R3M8",
+name: "Flan",
+ingredients: "catalan egg yolk custard, orange ice cream",
+restrictions: ['nutAllergy', 'vegetarian', 'pescatarian', 'kosher', 'glutenFree'],
+price: 6,
+picture: false
+}
+
+
+//the Original Dinerant
+var R4M1 = {
+reference: "R4M1",
+name: "Elvis Sighting",
+ingredients: "brioche, bacon, bananas, caramel sauce, candied nuts",
+restrictions: [false],
+price: 10,
+picture: true
+}
+
+var R4M2 = {
+reference: "R4M2",
+name: "Creme Brulee French Toast",
+ingredients: "brioche, eggs, milk",
+restrictions: ['vegetarian','pescatarian','nutAllergy','kosher','halal'],
+price: 9,
+picture: true
+}
+
+var R4M3 = {
+reference: "R4M3",
+name: "Tofu Hash",
+ingredients: "eggs, tofu, potatoes, seasonal vegetables, toast",
+restrictions: ['vegetarian','pescatarian','nutAllergy','kosher','halal'],
+price: 11,
+picture: true
+}
+
+var R4M4 = {
+reference: "R4M4",
+name: "Veggie Sausage Scramble",
+ingredients: "vegan sausage, eggs, cheddar, greens, tomato confit",
+restrictions: ['vegetarian','pescatarian','nutAllergy','kosher','halal'],
+price: 11,
+picture: true
+}
+
+var R4M5 = {
+reference: "R4M5",
+name: "Oregon Salad",
+ingredients: "greens, beets, pear, hazelnut, chevre, sherry vinaigrette",
+restrictions: ['glutenFree','vegetarian','pescatarian','kosher','halal'],
+price: 13.00,
+picture: true
+}
+
+//The Waffle Window
+var R5M1 = {
+reference: "R5M1",
+name: "Tomato B's",
+ingredients: "waffle (eggs, milk), brie, basil, tomato, peach jam",
+restrictions: ['vegetarian','pescatarian','nutAllergy','kosher','halal'],
+price: "?",
+picture: false
+}
+
+var R5M2 = {
+reference: "R5M2",
+name: "Farm Fusion",
+ingredients: "waffle, mushrooms, spinach, roasted pepper, tomato, chevre",
+restrictions: ['vegetarian','pescatarian','nutAllergy','kosher','halal'],
+price: "?",
+picture: true
+}
+
+var R5M3 = {
+reference: "R5M3",
+name: "Nutella and Fresh Banana",
+ingredients: "waffle, nutella, whipped cream, cinnamon sugar, fudge",
+restrictions: ['vegetarian','pescatarian','kosher','halal'],
+price: "?",
+picture: true
+}
+
+
+//Screen Door
+var R6M1 = {
+reference: "R6M1",
+name: "Crispy Fried Oysters",
+ingredients: "oysters, batter",
+restrictions: ['pescatarian','nutAllergy','halal'],
+price: 9.75,
+picture: true
+}
+
+var R6M2 = {
+reference: "R6M2",
+name: "Macroni and Cheese",
+ingredients: "macaroni, cheese",
+restrictions: ['vegetarian','pescatarian','nutAllergy','kosher','halal'],
+price: 4.75,
+picture: true
+}
+
+var R6M3 = {
+reference: "R6M3",
+name: "Hushpuppies",
+ingredients: "cornmeal, eggs, buttermilk, scallions",
+restrictions: ['vegetarian','pescatarian','nutAllergy','kosher','halal'],
+price: 6.50,
+picture: true
+}
+
+//Maurice
+
+//
+// var R7M = {
+// reference: "R7M",
+// name: "",
+// ingredients: "",
+// restrictions: [],
+// price: 0,
+// picture: true
+// }
---
+++ js/restaurant_data.js
@@ -0,0 +1,50 @@
+//this file is used to store the restaurant objects
+
+function Restaurant (name, reference, cuisine, services, rating, price_range, location, phone, restrictions, menuItems, restrictionMatch) {
+this.name = name;
+this.reference = reference;
+this.cuisine = cuisine;
+this.services = services;
+this.rating = rating;
+this.price_range = price_range;
+this.location = location;
+this.phone = phone;
+this.restrictions = restrictions;
+this.menuItems = menuItems;
+this.restrictionMatch = [];
+}
+
+//Natural Selection Restaurant Object
+var R1 = new Restaurant ("Natural Selection", "naturalSelection", ['european'], ["dinner"], 5, 3, "3033 NE Alberta Street, 97214", "503.328.2865", ["glutenFree", "vegetarian", "pescatarian", "vegan", "dairyFree", "hindu", "kosher", "halal"], [R1M1, R1M2, R1M3, R1M4, R1M5, R1M6, R1M7, R1M8]);
+
+//Bunk Sandwiches Restaurant Object
+var R2 = new Restaurant ("Bunk Sandwiches", "bunkSandwiches", ["sandwiches","american"], ['lunch','dinner','brunch'], 4, 1, "1028 SE Water Ave, 97214", "503.288.5883",  ["vegan", "vegetarian", "pescatarian", "nutAllergy", "hindu", "kosher", "halal" , "dairyFree"], [R2M1, R2M2, R2M3]);
+
+//Ataula Restaurant Object
+var R3 = new Restaurant ("Ataula", "ataula", ["spanish", "dinner"], 5, 3, "45.535922, -122.699614", "503.894.8904", ['glutenFree', 'vegetarian', 'pescatarian', 'vegan', 'nutAllergy', 'dairyFree', 'kosher','halal'], [R3M1, R3M2, R3M3, R3M4, R3M5, R3M6, R3M7, R3M8]);
+
+
+//
+//The Original Dinerant Restaurant Object
+var R4 = new Restaurant ("The Original Dinerant", "originalDinerant", ["american","diner"], ["breakfast","brunch","lunch","dinner"], 4, 2, "300 SW 6TH Ave 97204", "503.546.2666" ['glutenFree','vegetarian','pescatarian','nutAllergy','kosher','halal'], [R4M1, R4M2, R4M3, R4M4, R4M5]);
+
+// The Waffle Window Restaurant Object
+var R5 = new Restaurant ("The Waffle Window", "waffleWindow", ["american","fast-food"], ["breakfast","brunch","lunch","dinner"], 5,  1, "2624 NE Alberta St 97211", "503.265.8031",  ['vegetarian','pescatarian','nutAllergy','kosher','halal'], [R5M1, R5M2, R5M3]);
+
+//Screen Door Restaurant Object
+var R6 = new Restaurant ("Screen Door", "screenDoor", ["southern","cajun"], ["breakfast","brunch","lunch","dinner"], 5, 1, "2337 East Burnside St 97214", "503.542.0880", ['vegetarian','pescatarian','nutAllergy','kosher','halal'], [R6M1, R6M2, R6M3]);
+
+//Maurice Restaurant Object
+var R7 = new Restaurant("Maurice", "maurice", ["french","nordic"], ["lunch"], 4, 3, "921 SW Oak St, 97205", "503.224.9921",  ['glutenFree','vegetarian','pescatarian','vegan','dairyFree','hindu','kosher','halal','nutAllergy'], [R3M1, R3M2]);
+
+//Ox Restaurant Object
+var R8 = new Restaurant ("Ox", "ox", ["argentinian"], ["dinner"], 4, 3, "2225 NE MLK Blvd 97212", "503.284.3366", ['glutenFree','vegetarian','pescatarian','vegan','dairyFree','hindu','kosher','halal','nutAllergy'], [R3M1, R3M2])
+
+//Le Pigeon Restaurant Object
+var R9 = new Restaurant ("Le Pigeon",  "lePigeon", ["french"], ["dinner"], 5, 3, "738 E Burnside St 97214", "503.546.8796", ['glutenFree','vegetarian','pescatarian','dairyFree','hindu','kosher','halal','nutAllergy'], [R3M1, R3M2]);
+
+//Ava Gene's Restaurant Object
+var R10 = new Restaurant ("Ava Gene's", "avaGenes", ["italian"], ["dinner"], 4, 3, "3377 SE Division St 97202", ['glutenFree','vegetarian','pescatarian','vegan','dairyFree','hindu','kosher','halal','nutAllergy'], [R3M1, R3M2]);
+
+//Paadee Restaurant Object
+var R11 = new Restaurant ("Paadee", "paadee", ["thai"], ["lunch","dinner"],  3, 2, "6 SE 28th Ave 97214", "503.360.1453" ['glutenFree','vegetarian','pescatarian','vegan','dairyFree','hindu','kosher','halal','nutAllergy'], [R3M1, R3M2]);
