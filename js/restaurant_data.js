85c2cfafe2a070d6e48e5c54b42394dd19f961a7
mollieboots <student@epicodus.com>
2016-02-04 10:09:17 -0800

    add 7th restaurant

--- js/restaurant_data.js
+++ js/restaurant_data.js
@@ -18,7 +18,7 @@ this.restrictionMatch = [];
 var R1 = new Restaurant ("Natural Selection", "naturalSelection", ['european'], ["dinner"], 5, 3, "3033 NE Alberta Street, 97214", "503.328.2865", ["glutenFree", "vegetarian", "pescatarian", "vegan", "dairyFree", "hindu", "kosher", "halal"], [R1M1, R1M2, R1M3, R1M4, R1M5, R1M7, R1M8]);
 
 //Bunk Sandwiches Restaurant Object
-var R2 = new Restaurant ("Bunk Sandwiches", "bunkSandwiches", ["sandwiches", "american"], ['lunch','dinner','brunch'], 4, 1, "1028 SE Water Ave, 97214", "503.288.5883",  ["vegan", "vegetarian", "pescatarian", "nutAllergy", "hindu", "kosher", "halal" , "dairyFree"], [R2M1, R2M2, R2M3]);
+var R2 = new Restaurant ("Bunk Sandwiches", "bunkSandwiches", ["sandwiches", "american"], ['lunch','dinner','brunch'], 4, 1, "1028 SE Water Ave, 97214", "503.288.5883",  ["vegan", "vegetarian", "pescatarian", "nutAllergy", "hindu", "kosher", "halal" , "dairyFree"], [R2M1, R2M2, R2M3, R2M4, R2M5, R2M6, R2M7, R2M8]);
 
 //Ataula Restaurant Object
 var R3 = new Restaurant ("Ataula", "ataula", ["spanish"], ["dinner"], 5, 3, "1818 NW 23rd Pl 97210", "503.894.8904", ['glutenFree', 'vegetarian', 'pescatarian', 'vegan', 'nutAllergy', 'dairyFree', 'kosher','halal'], [R3M1, R3M2, R3M3, R3M4, R3M5, R3M6, R3M7, R3M8]);
@@ -26,16 +26,16 @@ var R3 = new Restaurant ("Ataula", "ataula", ["spanish"], ["dinner"], 5, 3, "181
 
 //
 //The Original Dinerant Restaurant Object
-var R4 = new Restaurant ("The Original Dinerant", "originalDinerant", ["american","diner"], ["breakfast","brunch","lunch","dinner"], 4, 2, "300 SW 6TH Ave 97204", "503.546.2666" ['glutenFree','vegetarian','pescatarian','nutAllergy','kosher','halal'], [R4M1, R4M2, R4M3, R4M4, R4M5]);
+var R4 = new Restaurant ("The Original Dinerant", "originalDinerant", ["american","diner"], ["breakfast","brunch","lunch","dinner"], 4, 2, "300 SW 6TH Ave 97204", "503.546.2666" ['glutenFree','vegetarian','pescatarian','nutAllergy','kosher','halal'], [R4M1, R4M2, R4M3, R4M4, R4M5, R4M6, R4M7, R4M8]);
 
 // The Waffle Window Restaurant Object
-var R5 = new Restaurant ("The Waffle Window", "waffleWindow", ["american","fast-food"], ["breakfast","brunch","lunch","dinner"], 5,  1, "2624 NE Alberta St 97211", "503.265.8031",  ['vegetarian','pescatarian','nutAllergy','kosher','halal'], [R5M1, R5M2, R5M3]);
+var R5 = new Restaurant ("The Waffle Window", "waffleWindow", ["american","fast-food"], ["breakfast","brunch","lunch","dinner"], 5,  1, "2624 NE Alberta St 97211", "503.265.8031",  ['vegetarian','pescatarian','nutAllergy','kosher','halal'], [R5M1, R5M2, R5M3, R5M4, R5M5, R5M6, R5M7, R5M8]);
 
 //Screen Door Restaurant Object
-var R6 = new Restaurant ("Screen Door", "screenDoor", ["southern","cajun"], ["breakfast","brunch","lunch","dinner"], 5, 1, "2337 East Burnside St 97214", "503.542.0880", ['vegetarian','pescatarian','nutAllergy','kosher','halal'], [R6M1, R6M2, R6M3]);
+var R6 = new Restaurant ("Screen Door", "screenDoor", ["southern","cajun"], ["breakfast","brunch","lunch","dinner"], 5, 1, "2337 East Burnside St 97214", "503.542.0880", ['vegetarian','pescatarian','nutAllergy','kosher','halal'], [R6M1, R6M2, R6M3, R6M4, R6M5, R6M6, R6M7, R6M8]);
 
 //Maurice Restaurant Object
-var R7 = new Restaurant("Maurice", "maurice", ["french","nordic"], ["lunch"], 4, 3, "921 SW Oak St, 97205", "503.224.9921",  ['glutenFree','vegetarian','pescatarian','vegan','dairyFree','hindu','kosher','halal','nutAllergy'], [R3M1, R3M2]);
+var R7 = new Restaurant("Luc Lac", "lucLac", ["vietnamese"], ["lunch", "dinner", "happy hour", "late night"], 4, 3, "835 SW 2nd Ave 97204", "503.222.0047",  ['glutenFree','vegetarian','pescatarian','vegan','dairyFree','hindu','kosher','halal','nutAllergy'], [R7M1, R7M2, R7M3, R7M4, R7M5, R7M6, R7M7, R7M8]);
 
 //Ox Restaurant Object
 var R8 = new Restaurant ("Ox", "ox", ["argentinian"], ["dinner"], 4, 3, "2225 NE MLK Blvd 97212", "503.284.3366", ['glutenFree','vegetarian','pescatarian','vegan','dairyFree','hindu','kosher','halal','nutAllergy'], [R3M1, R3M2])
